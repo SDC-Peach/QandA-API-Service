694a3e2de4c74a09d11ce054911af06f34766a6a
Ben Tanaka <BenT07891@gmail.com>
2022-11-03 17:56:25 -0500

    started on first API route for getting all questions

--- review_HackReactor_API.js
+++ review_HackReactor_API.js
@@ -3,19 +3,21 @@ const {TOKEN, URL} = require('./config.js');
 
 let prodID = '66644'
 prodID = '66642'
-axios({
-  method: 'get',
-  url: `${URL}/qa/questions/?product_id=${prodID}&page=1&count=${10000}`,
-  headers: { Authorization: TOKEN }
-})
-.then((val)=> {console.log(val.data)})
-.catch((err)=> {console.log(err)})
+// axios({
+//   method: 'get',
+//   url: `${URL}/qa/questions/?product_id=${prodID}&page=1&count=${10000}`,
+//   headers: { Authorization: TOKEN }
+// })
+// .then((val)=> {console.log(val.data)})
+// .catch((err)=> {console.log(err)})
 
-axios({
-  method: 'get',
-  url: `${URL}/qa/questions/?product_id=${prodID}&page=1&count=${10000}`,
-  headers: { Authorization: TOKEN }
-})
-.then((val)=> {console.log(val.data)})
-.catch((err)=> {console.log(err)})
+// axios({
+//   method: 'get',
+//   url: `${URL}/qa/questions/?product_id=${prodID}&page=1&count=${10000}`,
+//   headers: { Authorization: TOKEN }
+// })
+// .then((val)=> {console.log(val.data)})
+// .catch((err)=> {console.log(err)})
 
+axios.get('http://localhost:3000/qa/questions', {params: {product_id: prodID, count: 100}})
+.then((res)=> console.log(res.data))
